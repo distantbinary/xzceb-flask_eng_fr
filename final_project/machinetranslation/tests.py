f8b787774cef917ad33930ad8f9a8265ee547a7b
sp <staticpogona@gmail.com>
2023-04-16 15:58:59 -0400

    in progress

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,9 +3,13 @@ import translator
 # from machinetranslation import translator
 
 class TestTranslator(unittest.TestCase):
-    def test1(self):
-        self.assertEqual(translator.englishToFrench("Hello"), "Bonjour")
-        self.assertEqual(translator.frenchToEnglish("Bonjour"), "Hello")
+    def test_eng_to_fr(self):
+        self.assertEqual(translator.english_to_french("Hello"), "Bonjour")
+        self.assertNotEqual(translator.english_to_french("Hello"), "Hello")
+
+    def test_fr_to_eng(self):
+        self.assertEqual(translator.french_to_english("Bonjour"), "Hello")
+        self.assertNotEqual(translator.french_to_english("Bonjour"), "Bonjour")
 
 if __name__ == '__main__':
     unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,35 +1,42 @@
-# ```py
+"""
+This module translate from English to French and vice versa
+"""
 import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# apikey = os.environ['iuXXelXUFt-7ICyoxq434BrneQuVLtx_Z2p0ObLULLjs']
-# url = os.environ['https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/63fe1554-b07f-4d73-86d3-5a10970e13f6']
-# ```
-apikey='iuXXelXUFt-7ICyoxq434BrneQuVLtx_Z2p0ObLULLjs'
-url='https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/63fe1554-b07f-4d73-86d3-5a10970e13f6'
+APIKEY='iuXXelXUFt-7ICyoxq434BrneQuVLtx_Z2p0ObLULLjs'
+URL='https://api.au-syd.language-translator.watson.cloud.ibm.com/'\
+    'instances/63fe1554-b07f-4d73-86d3-5a10970e13f6'
 
-authenticator = IAMAuthenticator(apikey)
+authenticator = IAMAuthenticator(APIKEY)
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url(url)
-def englishToFrench(englishText):
-    translation = language_translator.translate(
-        text=englishText,
-        model_id='en-fr').get_result()
-    frenchText = translation['translations'][0]['translation']
-    return frenchText
+language_translator.set_service_url(URL)
 
-def frenchToEnglish(frenchText):
+def english_to_french(english_text):
+    """
+    This function translates English to French
+    """
     translation = language_translator.translate(
-        text=frenchText,
+        text=english_text,
+        model_id='en-fr').get_result()
+    french_text = translation['translations'][0]['translation']
+    return french_text
+
+def french_to_english(french_text):
+    """
+    This function translates French to English
+    """
+    translation = language_translator.translate(
+        text=french_text,
         model_id='fr-en').get_result()
-    englishText = translation['translations'][0]['translation']
-    return englishText
+    english_text = translation['translations'][0]['translation']
+    return english_text
